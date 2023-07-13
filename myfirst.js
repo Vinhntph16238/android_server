3d861a51ffa2b742dd53bf898e51e9654c84f863
vinhntph16238 <vinhntph16238@fpt.edu.vn>
2023-07-13 15:44:06 +0700

    lab2bai3

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,19 @@
 var http = require('http');
+var maytinh = require('./maytinh');
 
+function timeMillis() {
+    return Date();
+}
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World! ban vinhntph');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    var x = 17;
+    var y = 28;
+    var tong = maytinh.phepcong(x, y);
+
+    console.log(timeMillis());
+
+    res.end(`Tong 2 so ${x} va ${y} la: ${tong}`);
 
 }).listen(8080);
